4a9abf98e1fa54ac42008a7cc2302cd53dab951c
kishore2205 <kishorekumargmsk@gmail.com>
2025-07-17 15:32:12 +0530

    changed ALLOWED_HOST

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import dj_database_url
 import os
 from pathlib import Path
 
@@ -85,8 +84,6 @@ DATABASES ={
             }
 }
 
-database_url = os.environ.get('DATABASE_URL')
-DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
