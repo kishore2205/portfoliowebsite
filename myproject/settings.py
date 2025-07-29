0daee8f354776fae0df59c50b876b12abd15e7d0
kishorekumar <kishorekumargmsk@gmail.com>
2025-07-29 16:12:16 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = [51.20.53.33,'*']
+ALLOWED_HOSTS =[]
 
 
 # Application definition
