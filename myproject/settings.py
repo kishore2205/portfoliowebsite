68b7779e844ca3d36d53aae5abab6d5b27b15a25
kishorekumar <kishorekumargmsk@gmail.com>
2025-07-29 16:24:59 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['51.20.53.33', 'portfoliowebsite-5mv9.onrender.com']
+ALLOWED_HOSTS = ['portfoliowebsite-5mv9.onrender.com']
 
 
 
